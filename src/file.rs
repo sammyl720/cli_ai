e50a61f533433f2f4fef59ddcd42a182965c28a1
sam <sam.leider@hotmail.com>
2024-06-02 22:18:26 -0400

    save generated readme files to documents folder

--- src/file.rs
+++ src/file.rs
@@ -2,17 +2,34 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-const OUTPUT_DIR: &str = "./output";
+use directories::UserDirs;
 
-pub fn write_to_file(file_name: &str, content: &str) -> std::io::Result<()> {
-    let path = Path::new(OUTPUT_DIR).join(&Path::new(file_name));
+pub struct UserDocumentsWriter;
 
-    let mut file = File::create(&path)?;
-    file.write_all(content.as_bytes())?;
-    println!("file saved to '{:?}'", path);
-    Ok(())
+impl FileWriter for UserDocumentsWriter {
+    fn write(path: &Path, content: &str) -> std::io::Result<String> {
+        if let Some(user_dirs) = UserDirs::new() {
+            if let Some(document_path) = user_dirs.document_dir() {
+                let full_path = document_path.join(path);
+                return write_file(&full_path, content);
+            }
+        }
+        write_file(path, content)
+    }
+}
+trait FileWriter {
+    fn write(path: &Path, content: &str) -> std::io::Result<String> {
+        write_file(path, content)
+    }
 }
 
-pub fn write_readme(file_name: &str, content: &str) -> std::io::Result<()> {
-    return write_to_file(format!("markdown/{}.md", file_name).as_str(), content);
+pub fn write_file(path: &Path, content: &str) -> std::io::Result<String> {
+    let mut file = File::create(&path)?;
+    file.write_all(content.as_bytes())?;
+    let result = format!("file saved to '{:?}'", path);
+    Ok(result)
+}
+pub fn write_readme(file_name: &str, content: &str) -> std::io::Result<String> {
+    let file_path = format!("{}.md", file_name);
+    return UserDocumentsWriter::write(Path::new(file_path.as_str()), content);
 }
